4cabaad0046f992d0ee4586365c2d74edea48f48
kit rae <kitraeissogood25@gmail.com>
2026-03-06 02:14:45 +0000

    Revert "wire vulkan init + gdraw fixes"
    This reverts commit 4477781b25d24fa0eba379e745e96b16ad90afbb

--- Minecraft.Client/Windows64/Windows64_Minecraft.cpp
+++ Minecraft.Client/Windows64/Windows64_Minecraft.cpp
@@ -858,7 +858,7 @@ HRESULT InitDevice()
 	vp.TopLeftY = 0;
 	g_pImmediateContext->RSSetViewports( 1, &vp );
 
-	RenderManager.Initialise(g_hWnd, width, height);
+	RenderManager.Initialise(g_pd3dDevice, g_pSwapChain);
 
 	return S_OK;
 }
@@ -1029,7 +1029,7 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 #endif
 	app.loadMediaArchive();
 
-	RenderManager.Initialise(g_hWnd, g_iScreenWidth, g_iScreenHeight);
+	RenderManager.Initialise(g_pd3dDevice, g_pSwapChain);
 
 	app.loadStringTable();
 	ui.init(g_pd3dDevice,g_pImmediateContext,g_pRenderTargetView,g_pDepthStencilView,g_iScreenWidth,g_iScreenHeight);
